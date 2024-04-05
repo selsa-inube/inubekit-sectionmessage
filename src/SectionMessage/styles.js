6d9e137766cc42d11490dd61005fe9931650242a
cmarin001 <cmarin6051@gmail.com>
2024-04-05 18:26:11 -0500

    fix(sectionMessage): adjust color tokens as it should be

--- src/SectionMessage/styles.js
+++ src/SectionMessage/styles.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import { inube } from "@inubekit/foundations";
 
 const StyledSectionMessage = styled.div`
-  background-color: ${({ theme, $appearance }) => {
-    theme?.sectionMessage?.[$appearance]?.background?.color;
-    inube.sectionMessage[$appearance].background.color;
-  }};
+  background-color: ${({ theme, $appearance }) =>
+    theme?.sectionMessage?.[$appearance]?.background?.color ||
+    inube.sectionMessage[$appearance].background.color};
   width: ${($isMessageResponsive) => ($isMessageResponsive ? "auto" : "400px")};
-  height: auto;
+  height: 100%;
+  max-height: 78px;
   border-radius: 4px;
   box-shadow:
     0px 1px 2px rgba(0, 0, 0, 0.3),
@@ -16,4 +16,11 @@ const StyledSectionMessage = styled.div`
   overflow-wrap: anywhere;
 `;
 
-export { StyledSectionMessage };
+const StyledCountdownBarContainer = styled.div`
+  margin-top: -4px;
+  > div {
+    border-radius: 4px;
+  }
+`;
+
+export { StyledSectionMessage, StyledCountdownBarContainer };
